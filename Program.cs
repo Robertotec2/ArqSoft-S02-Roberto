589aa53ac48bb717c9088fefc15e3c6b927223fd
Robertotec2 <robertobalmessol1s@gmail.com>
2026-05-12 13:00:24 -0600

    refactor: limpiar Program.cs con inyeccion de dependecias

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿var juego = new Ahorcado.Juego();
-juego.Jugar(); 
+﻿using Ahorcado;
+
+var repositorio = new PalabrasEnMemoria();
+bool jugarDeNuevo = true;
+
+while (jugarDeNuevo)
+{
+    var motor = new MotorAhorcado(repositorio);
+    var ui = new ConsolaUI(motor);
+
+    while (!motor.Ganado() && !motor.Perdido())
+    {
+        ui.MostrarTablero();
+        char letra = ui.PedirLetra();
+
+        if (letra == ' ') continue;
+
+        if (motor.LetraYaUsada(letra))
+        {
+            ui.MostrarMensaje("Ya usaste esa letra. Presiona cualquier tecla...");
+            Console.ReadKey();
+            continue;
+        }
+
+        motor.RegistrarLetra(letra);
+    }
+
+    ui.MostrarTablero();
+
+    if (motor.Ganado())
+        ui.MostrarMensaje($"\n¡Ganaste! La palabra era: {motor.PalabraSecreta}");
+    else
+        ui.MostrarMensaje($"\nPerdiste. La palabra era: {motor.PalabraSecreta}");
+
+    jugarDeNuevo = ui.PreguntarOtraVez();
+}
