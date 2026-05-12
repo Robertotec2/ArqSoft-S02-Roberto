b47ec1545be7fff5b55d06e03cb93ee1fdd34fc9
Robertotec2 <robertobalmessol1s@gmail.com>
2026-05-12 12:55:28 -0600

    refactor: extraer MotorAhorcado

---
+++ MotorAhorcado.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ahorcado
+{
+    public class MotorAhorcado
+    {
+        private readonly string _palabraSecreta;
+        private readonly List<char> _letrasUsadas = new();
+        private int _intentosRestantes = 6;
+
+        // Propiedades
+        public string PalabraSecreta => _palabraSecreta;
+        public List<char> LetrasUsadas => _letrasUsadas;
+        public int IntentosRestantes => _intentosRestantes;
+
+        public MotorAhorcado(IRepositorioPalabras repositorio)
+        {
+            _palabraSecreta = repositorio.ObtenerPalabraAleatoria();
+        }
+
+        // Métodos de lógica
+        public bool LetraYaUsada(char letra) => _letrasUsadas.Contains(letra);
+
+        public bool EsLetraCorrecta(char letra) => _palabraSecreta.Contains(letra);
+
+        public void RegistrarLetra(char letra)
+        {
+            if (LetraYaUsada(letra)) return;
+
+            _letrasUsadas.Add(letra);
+
+            if (!EsLetraCorrecta(letra))
+                _intentosRestantes--;
+        }
+
+        public bool Ganado()
+        {
+            // Usando LINQ para mayor claridad: ¿Están todas las letras de la palabra en la lista de usadas?
+            return _palabraSecreta.All(c => _letrasUsadas.Contains(c));
+        }
+
+        public bool Perdido() => _intentosRestantes <= 0;
+    }
+}
